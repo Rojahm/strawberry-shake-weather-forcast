f350a8af32e7f525477605d73dcae2e721c9f6ca
Rojahm <100220824+Rojahm@users.noreply.github.com>
2022-11-25 21:28:11 +0330

    forecast API

--- src/index.js
+++ src/index.js
@@ -1,48 +1,93 @@
 // time and date part
-let date = new Date();
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let days = [
-  "Sunday",
-  "Monday",
-  "Thuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+function formatDateTime() {
+  let date = new Date();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Thuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-document.querySelector("#day").innerHTML = days[date.getDay()];
-document.querySelector("#month").innerHTML = months[date.getMonth()];
-document.querySelector("#year").innerHTML = date.getFullYear();
-let hour = date.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
+  document.querySelector("#day").innerHTML = days[date.getDay()];
+  document.querySelector("#month").innerHTML = months[date.getMonth()];
+  document.querySelector("#year").innerHTML = date.getFullYear();
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  document.querySelector("#hour").innerHTML = `${hour}:${minute}`;
 }
-let minute = date.getMinutes();
-if (minute < 10) {
-  minute = `0${minute}`;
-}
-document.querySelector("#hour").innerHTML = `${hour}:${minute}`;
+formatDateTime();
 
+// forecast bit
+function formatTimestampToDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Thu", "Wed", "Thr", "Fri", "Sat"];
+  return days[day];
+}
+function forecastTemplate(response) {
+  console.log(response.data.daily);
+  let forecastDay = response.data.daily;
+  console.log(forecastDay);
+  let forecastHtml = document.querySelector("#forecast");
+  let template = "";
+  forecastDay.forEach(function (day, index) {
+    // console.log("hi", day.time);
+
+    template += `<div class="card">
+  <div class="title"><h3>${formatTimestampToDay(day.time)}</h3></div>
+  <div class="icon">
+  <img
+  src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+  alt="icon"
+  />
+  </div>
+  <div class="tempreture">
+  <h3 id="max-temperature">${Math.round(day.temperature.maximum)}</h3>
+  <h3 id="min-temperature">${Math.round(day.temperature.minimum)}</h3>
+  </div>
+  </div>`;
+  });
+  forecastHtml.innerHTML = template;
+}
+
+function displayForecast(coordinates) {
+  let latitude = coordinates.latitude;
+  let longitude = coordinates.longitude;
+  let apiEndpoint = "https://api.shecodes.io/weather/v1/forecast";
+  let apiUrl = `${apiEndpoint}?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+  axios.get(apiUrl).then(forecastTemplate);
+}
 // temperature part
 let celsius = null;
 let apiKey = "a7c7f51a8a5abc24e0tb69o4ff6018a3";
 let apiEndpoint = "https://api.shecodes.io/weather/v1/current";
 let apiQuery = "Berlin";
 let apiUrl = `${apiEndpoint}?query=${apiQuery}&key=${apiKey}`;
+let coordinates = [];
+let timestamp = null;
 
 function showTempreture(response) {
   document.querySelector("#city").innerHTML = response.data.city;
@@ -67,6 +112,12 @@ function showTempreture(response) {
     response.data.temperature.pressure
   );
   celsius = Math.round(response.data.temperature.current);
+
+  coordinates = response.data.coordinates;
+  displayForecast(coordinates);
+
+  timestamp = response.data.time;
+  formatTimestampToDay(timestamp);
 }
 axios.get(apiUrl).then(showTempreture);
 
@@ -80,6 +131,7 @@ function searchForCity(event) {
 let form = document.querySelector("#search");
 form.addEventListener("submit", searchForCity);
 
+//current position weather
 function showCurrentWeather(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -117,31 +169,3 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiuslink = document.querySelector("#celsius");
 celsiuslink.addEventListener("click", convertToCelsius);
-
-function forecastTemplate() {
-  let forecastHtml = document.querySelector("#forecast");
-  let days = ["Sun", "Mon", "Thu", "Wed", "Thr", "Fri", "Sat"];
-  let template = "";
-  days.forEach(function (day, index) {
-    if (index < 6) {
-      template =
-        template +
-        `<div class="card">
-  <div class="title"><h3>${day}</h3></div>
-  <div class="icon">
-  <img
-  src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
-  alt="icon"
-  />
-  </div>
-  <div class="tempreture">
-  <h3 id="max-temperature">11</h3>
-  <h3 id="min-temperature">10</h3>
-  </div>
-  </div>`;
-    }
-  });
-  forecastHtml.innerHTML = template;
-}
-
-forecastTemplate();
